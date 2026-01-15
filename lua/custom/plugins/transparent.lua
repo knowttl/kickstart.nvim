a2f1baca44a40d8623b957e182cbca51f86a7fd0
Brytton <brytton.tsai.2010@gmail.com>
2026-01-14 21:04:33 -0800

    updated key bindings

--- lua/custom/plugins/transparent.lua
+++ lua/custom/plugins/transparent.lua
@@ -2,5 +2,6 @@ return {
   'https://github.com/xiyaowong/transparent.nvim.git',
   config = function()
     -- require('transparent').clear_prefix 'lualine'
+    -- require('transparent').clear_prefix 'ToggleTerm'
   end,
 }
