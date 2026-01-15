6650e0851c5963d343cfb33e90e18f3964c5083f
Brytton <brytton.tsai.2010@gmail.com>
2026-01-14 21:59:29 -0800

    force transparent backgroundf

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -3,11 +3,12 @@ return {
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   config = function()
     local auto_theme = require 'lualine.themes.auto'
-    auto_theme.normal.c.bg = 'None'
+    auto_theme.normal.c.bg = 'NONE'
     require('lualine').setup {
       options = {
         theme = auto_theme,
       },
     }
+    vim.api.nvim_set_hl(0, 'LualineNormalC', { bg = 'NONE' })
   end,
 }
