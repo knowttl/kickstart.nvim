20759d3ab8c6ae021b67052d5e9b0ae2dec4908f
knowttl <knowttl42@gmail.com>
2026-06-21 02:10:02 +0000

    fixed some transparency issues

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -2,13 +2,10 @@ return {
   'nvim-lualine/lualine.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   config = function()
-    local auto_theme = require 'lualine.themes.auto'
-    auto_theme.normal.c.bg = 'NONE'
     require('lualine').setup {
       options = {
-        theme = auto_theme,
+        theme = 'auto',
       },
     }
-    vim.api.nvim_set_hl(0, 'LualineNormalC', { bg = 'NONE' })
   end,
 }
--- lua/custom/plugins/transparent.lua
+++ lua/custom/plugins/transparent.lua
@@ -1,10 +1,49 @@
 return {
   'https://github.com/xiyaowong/transparent.nvim.git',
+  lazy = false,
+  init = function()
+    local cache_path = vim.fn.stdpath 'data' .. package.config:sub(1, 1) .. 'transparent_cache'
+    if vim.fn.filereadable(cache_path) == 0 and vim.g.transparent_enabled == nil then
+      vim.g.transparent_enabled = true
+    end
+  end,
   config = function()
-    require('transparent').setup {
-      on_clear = function() end,
+    local transparent = require 'transparent'
+
+    transparent.setup {
+      extra_groups = {
+        'NormalFloat',
+        'FloatBorder',
+        'FloatTitle',
+        'FoldColumn',
+        'Folded',
+        'MsgArea',
+        'WinSeparator',
+        'TabLine',
+        'TabLineFill',
+        'TelescopeNormal',
+        'TelescopeBorder',
+        'TelescopePromptNormal',
+        'TelescopePromptBorder',
+        'TelescopeResultsNormal',
+        'TelescopeResultsBorder',
+        'TelescopePreviewNormal',
+        'TelescopePreviewBorder',
+        'WhichKeyFloat',
+        'LazyNormal',
+        'MasonNormal',
+        'BlinkCmpMenu',
+        'BlinkCmpMenuBorder',
+        'BlinkCmpDoc',
+        'BlinkCmpDocBorder',
+        'BlinkCmpSignatureHelp',
+        'BlinkCmpSignatureHelpBorder',
+      },
     }
-    -- require('transparent').clear_prefix 'lualine'
-    -- require('transparent').clear_prefix 'ToggleTerm'
+
+    transparent.clear_prefix 'BufferLine'
+    transparent.clear_prefix 'NeoTree'
+    transparent.clear_prefix 'lualine'
+    transparent.clear_prefix 'ToggleTerm'
   end,
 }
